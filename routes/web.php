a26da7791aeb8fc8a5c960afa8e165a57304f71f
fredy.siswanto <fredy.siswanto@komunal.co.id>
2024-11-22 22:35:13 +0700

    setup

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('dashboard', 'HomeController@dashboard')->name('dashboard');
+// Route::group(['middleware' => ['auth']], function () {
+//     Route::get('/home', 'HomeController@index')->name('home');
+//     Route::get('/home', 'HomeController@index')->name('home');
+//     Route::get('dashboard', 'HomeController@dashboard')->name('dashboard');
+// });
